7bfcfaad3518f4091508d28452a7403aa8c36137
Artem-Stryzhakov <Phernander1@gmail.com>
2022-11-19 22:27:12 +0200

    Default Commit

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const __dirname = path.resolve()
 
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs")
 app.set("views", path.resolve(__dirname, "ejs"))
