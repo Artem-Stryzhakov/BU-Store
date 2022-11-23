f4c2d418ec13054ecd6eeb4527b782e91a0e4f81
Artem-Stryzhakov <Phernander1@gmail.com>
2022-11-23 22:26:14 +0200

    Authorization has been maded...

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import mongoose from "mongoose";
-import path from 'path'
+import path, {resolve} from 'path'
 import multer from 'multer'
 
 import {validationResult} from 'express-validator'
 import {registerValidation} from './validations/auth.js'
+
 import UserModel from './models/User.js'
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from './Controllers/UserController.js'
 
 import {deleteData, getData, pushData, updateData, uploadImage} from './Controllers/Requests.js'
 
 // ===== DATABASE CONNECTION ===== //
-mongoose.connect('mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongodb.net/BU-store?retryWrites=true&w=majority')
     .then(() => console.log("DB is OK"))
     .catch((err) => console.log('DB error', err));
 // =============================== //
@@ -47,28 +50,9 @@ app.get('/features', (req, res) => {
     res.render('features', {title: "Main Features", active: "features"})
 })
 
-app.post('/auth/register', registerValidation, async(req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-    const user = await doc.save();
-
-    res.json(user)
-    console.log(user.createdAt)
-})
+app.post('/auth/login', UserController.login)
+app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/me', checkAuth, UserController.getMe)
 
 app.get('/getData', getData)
 app.post('/pushData', pushData)
